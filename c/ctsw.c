808d6fbb7976e6ed0f6004970455463b2d7bed50
Cary <melstrom@gmail.com>
2012-10-04 12:22:43 -0700

    Init; added assembly code

--- c/ctsw.c
+++ c/ctsw.c
@@ -9,3 +9,39 @@
    compiler/assembler options or issue directives to permit usage of Intel's
    assembly language conventions.
 */
+
+void contextswitch(void * currentProccess)
+{
+  __asm__ (
+      "pushf;" //push kernel state onto the stack
+      "pusha;"
+      "mov1 %esp, kern_stack;" //save kernel stack pointer
+      "mov1 proc_stack, $esp;" //switch to process stack
+      //save return value of system call
+      "popa;" //pop process state from the process stack
+      "iret;");
+//entry point:
+   __asm__ (
+       "pusha;" //* push process state onto process stack
+       "mov1 %esp, proc_stack;" //* save process stack pointer
+       "mov1 kern_stack, %esp;" //* switch to kernel stack
+       //* save system call arguments
+       "popa; popf;"); //* pop kernel state from kernel stack
+
+   return; //* return from context switcher
+
+}
+
+/*
+ * void contexSwitch(É) {
+  save kernel state & stack ptr
+  recover process stack ptr &
+          registers
+
+  iret
+COMMON_ISR:
+  save process registers and
+       stack pointer
+  recover kernel stack ptr & state
+}
+ */
